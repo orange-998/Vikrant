e40b64c0daa8673cc58e19a6f4a7c764221d00a5
orange-998 <161716934+orange-998@users.noreply.github.com>
2024-02-29 17:34:55 +0530

    Add files via upload

---
+++ Assingment_type_con.py
@@ -0,0 +1,34 @@
+# change int to float
+a=int(input("enter a no"))
+print(float(a))
+
+# change float to int
+
+a=float(input("enter a no"))
+print(int(a))
+
+# change int to string
+
+a=int(input("enter a no"))
+print(str(a))
+
+# change list to Tuple
+
+a= [1,2,3,5,"vikrant", 20, 40]
+print(tuple(a))
+
+# change Tuple to list
+
+a= (1,2,3,5,"vikrant", 20, 40)
+print(list(a))
+
+# change Decimal to binary
+
+a=int(input("enter a no"))
+print(bin(a))
+
+# change non Zero to Boolen
+
+a= "Vikrant"
+print(bool(a))
+
