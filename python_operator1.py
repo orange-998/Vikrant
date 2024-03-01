f5fc9a70b82952e8676bae3ddc46a328af11b8ba
orange-998 <161716934+orange-998@users.noreply.github.com>
2024-03-01 15:06:27 +0530

    Add files via upload

---
+++ python_operator1.py
@@ -0,0 +1,76 @@
+# Q1 calculate Add,sub,mul,div
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# Q2 Assingement opperator
+
+c=5
+print(c)
+c = c+3
+d= c+ 4
+print(c)
+print(d)
+
+# Q3 compare two values
+
+if a == b:
+ print(True)
+
+else:
+ print(False)
+
+if a > b:
+ print(True)
+
+else:
+ print(False)
+
+#Q4 conditions using logical operators.
+
+print(True & True)
+print(True & False)
+
+print(True or True)
+print(True or False)
+
+print(not True)
+print(not False)
+
+# Q5 identity of variables.
+print(a is b)
+
+r="vikrant"
+s=r
+
+print(s is r)
+
+# Q6 bitwise operations on any two integers.
+
+print(2 & 2)
+print(a & b)
+print(a ^ b)
+print(bin(5))
+
+# Q7  Use unary operators to change the sign of a number.
+
+print(~3)
+print(~7)
+
+# Q8 ternary operator to assign values based on conditions
+
+xyz= a if a < b else b
+print(xyz)
+
+
+
+
+
+
+
